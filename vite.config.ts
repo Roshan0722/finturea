9ac736488ebb16170cad0194dab5673785510ea9
Roshan0722 <roshan.namdev@saa.ai>
2026-04-16 15:10:18 +0530

    Fixed routing issue.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// export default defineConfig({
+// content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//    theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ["DM Sans", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [react(),tailwindcss(),
+// ],
+// })
 export default defineConfig({
-content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["DM Sans", "sans-serif"],
-      },
-    },
-  },
-  plugins: [react(),tailwindcss(),
-],
+  plugins: [react()],
+  base: '/', // important for Vercel
 })
